9aa70e6a008a517401dae1083c03a204b31b8a99
Helena Freire <helenamcfreire@gmail.com>
2013-01-20 10:44:13 -0200

    Comentei o trecho de código que buscava no google search image pra cada tutorial.

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -21,15 +21,15 @@ class Tutorial < ActiveRecord::Base
     self.errors.add :base, "Tutorial must have at least one category. Maybe you reported an absent category." if self.categories.blank?
   end
 
-  def first_category
-    categories.first
-  end
-
-  def thumbnail
-    url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBjCmHNmgYtzjImhQRDdD8uC5eDK9Uedhc&cx=868056535109.apps.googleusercontent.com&searchType=image&q="+first_category.name
-    response = HTTParty.get(url)
-    json = JSON.parse(response.body)
-    json["items"][0]["link"]
-  end
+  #def first_category
+  #  categories.first
+  #end
+  #
+  #def thumbnail
+  #  url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBjCmHNmgYtzjImhQRDdD8uC5eDK9Uedhc&cx=868056535109.apps.googleusercontent.com&searchType=image&q="+first_category.name
+  #  response = HTTParty.get(url)
+  #  json = JSON.parse(response.body)
+  #  json["items"][0]["link"]
+  #end
 
 end
